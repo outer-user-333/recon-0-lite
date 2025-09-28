2b30710e7af016e7098089e8667a485c78c397ec
sujal <sujalshankar12345@gmail.com>
2025-09-28 20:14:08 +0530

    web config

--- src/main/java/com/example/Recon0/config/SecurityConfig.java
+++ src/main/java/com/example/Recon0/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -52,4 +53,6 @@ public class SecurityConfig {
         authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
         return authenticationManagerBuilder.build();
     }
+
+
 }
